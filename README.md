afe6f5839c1095e28ad35802b4da3a6aae66a211
shivendrashahi <shahi.husky@gmail.com>
2019-09-14 16:29:43 -0400

    Add assignment 1 to the project

---
+++ README.md
@@ -0,0 +1,1 @@
+Assignment 1 of AED - JAVA Swing - Car
---
+++ src/Business/Car.java
@@ -0,0 +1,170 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author Shivendra
+ */
+public class Car {
+    
+    private String model;
+    private String color;
+    private String serialNum;
+    private String price;
+    private String wheelType;
+    private String transmissionType;
+    private String fuelType;
+    private String styleType;
+    private String horsepower;
+    private String mileage;
+    private String engineType;
+    private String noOfAirbag;
+    private String noOfGear;
+    private String height;
+    private String groundClearance;
+    private String interiorType;
+    private String image;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSerialNum() {
+        return serialNum;
+    }
+
+    public void setSerialNum(String serialNum) {
+        this.serialNum = serialNum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getWheelType() {
+        return wheelType;
+    }
+
+    public void setWheelType(String wheelType) {
+        this.wheelType = wheelType;
+    }
+
+    public String getTransmissionType() {
+        return transmissionType;
+    }
+
+    public void setTransmissionType(String transmissionType) {
+        this.transmissionType = transmissionType;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public String getStyleType() {
+        return styleType;
+    }
+
+    public void setStyleType(String styleType) {
+        this.styleType = styleType;
+    }
+
+    public String getHorsepower() {
+        return horsepower;
+    }
+
+    public void setHorsepower(String horsepower) {
+        this.horsepower = horsepower;
+    }
+
+    public String getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(String mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
+
+    public String getNoOfAirbag() {
+        return noOfAirbag;
+    }
+
+    public void setNoOfAirbag(String noOfAirbag) {
+        this.noOfAirbag = noOfAirbag;
+    }
+
+    public String getNoOfGear() {
+        return noOfGear;
+    }
+
+    public void setNoOfGear(String noOfGear) {
+        this.noOfGear = noOfGear;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getGroundClearance() {
+        return groundClearance;
+    }
+
+    public void setGroundClearance(String groundClearance) {
+        this.groundClearance = groundClearance;
+    }
+
+    public String getInteriorType() {
+        return interiorType;
+    }
+
+    public void setInteriorType(String interiorType) {
+        this.interiorType = interiorType;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+   
+    
+}
